fa27eb9bbb6fa6691dbb71fbba33f741e060ca8c
Lucas Quintanilha <l.quitan@gmail.com>
2020-06-25 14:35:46 -0300

    Updates

--- README.md
+++
@@ -1,5 +0,0 @@
-<h1 align="center">
-  Monsters
-  <br>
-  <img src="/src/assets/preview.png"/>
- </h1>
---
+++ precache-manifest.7514560e439b90e89ca11fc05955c5ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ea204ef07c564fc38667b8d1b3069f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff6136964ea37b0ca3d5",
+    "url": "/static/css/main.68bf45ad.chunk.css"
+  },
+  {
+    "revision": "7b555debf535f32f782e",
+    "url": "/static/js/2.9fc7c5d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9fc7c5d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff6136964ea37b0ca3d5",
+    "url": "/static/js/main.0b1a4d49.chunk.js"
+  },
+  {
+    "revision": "a55fe9c0b312e2774e66",
+    "url": "/static/js/runtime-main.80c5e1e7.js"
+  }
+]);
